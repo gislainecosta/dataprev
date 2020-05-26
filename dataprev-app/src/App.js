168f00ef85c2e8ffcd0eac849cb44c1138c80251
Gislaine Costa Pereira <62241763+gislainecosta@users.noreply.github.com>
2020-05-26 17:29:52 -0300

    Rotas de página

--- dataprev-app/src/App.js
+++ dataprev-app/src/App.js
@@ -1,25 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Switch, Route, BrowserRouter } from "react-router-dom";
 import './App.css';
+import Login from "./Pages/Login"
+import HomeUser from "./Pages/HomeUser"
+import Simulate from "./Pages/Simulate"
+import PerfilUser from "./Pages/PerfilUser"
+import Preferences from "./Pages/Preferences"
+import Tutorial from "./Pages/Tutorial"
+import UserGaming from "./Pages/UserGaming"
 
-function App() {
+
+const App =() => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Login  />
+        </Route>
+
+        <Route exact path="/simulate">
+          <Simulate />
+        </Route>
+        
+        <Route exact path="/home">
+          <HomeUser />
+        </Route>
+        
+        <Route exact path ="/perfil" >
+          <PerfilUser />
+        </Route>
+
+        <Route exact path ="/preferencias" >
+          <Preferences />
+        </Route>
+      
+        <Route exact path ="/tutorial" >
+          <Tutorial />
+        </Route>
+        <Route exact path ="/perfilxp" >
+          <UserGaming />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
